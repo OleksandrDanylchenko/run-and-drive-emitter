9ed22bed4315595a150b09d94700b099883805e5
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-06 17:18:24 +0200

    feat: add pwa vite plugin settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [
+    reactRefresh(),
+    tsconfigPaths(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Emitter | Run-And-Drive',
+        short_name: 'Emitter',
+        description: 'Emits the location and indications of the car',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        dir: 'ltr',
+        orientation: 'portrait',
+        start_url: '/',
+        lang: 'en',
+      },
+    }),
+  ],
 });
