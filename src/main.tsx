c7251d2ebee97bb6d13461d8eb35282a5498b50e
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-06 14:00:24 +0200

    feat: add MUI styles setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+import { CssBaseline, StyledEngineProvider, Typography } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <CssBaseline enableColorScheme />
+      <Typography variant={'h2'}>Hello, emitter</Typography>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
