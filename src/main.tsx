e39430724ff5c03b9451423e35bee4a932106273
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-06 16:44:25 +0200

    feat: add basic configuration for the store

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,19 @@ import '@fontsource/roboto/700.css';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import Routing from './navigation/Routing';
+import { store } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <CssBaseline enableColorScheme />
-      <Routing />
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline enableColorScheme />
+        <Routing />
+      </StyledEngineProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
