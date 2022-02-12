a5563a7fd16633a693be55520925d3b71296540a
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-12 22:33:57 +0200

    feat: add support button

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,28 @@
 import SettingsInputAntennaIcon from '@mui/icons-material/SettingsInputAntenna';
+import SupportIcon from '@mui/icons-material/Support';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import React, { FC } from 'react';
 
-import { AppButton } from './styles';
+import { AppButton, AppButtonWrapper } from './styles';
 
 const Dashboard: FC = () => {
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Button variant="contained" disableElevation css={AppButton}>
-            <SettingsInputAntennaIcon />
-            <Typography variant="h6">Emitter</Typography>
-          </Button>
+          <Box css={AppButtonWrapper}>
+            <Button startIcon={<SettingsInputAntennaIcon />} css={AppButton}>
+              Emitter
+            </Button>
+          </Box>
+          <IconButton aria-label="Contact support" color="inherit">
+            <SupportIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Container maxWidth="sm"></Container>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,11 +1,12 @@
 import { css } from '@emotion/react';
 import { flexbox } from '@styles/mixins';
-import { pxToRem } from '@styles/utils';
+
+export const AppButtonWrapper = css`
+  ${flexbox({ grow: 1 })}
+`;
 
 export const AppButton = css`
-  ${flexbox({
-    gap: pxToRem('15px'),
-  })}
-
+  font-size: 1.2em;
+  color: inherit;
   text-transform: none;
 `;
