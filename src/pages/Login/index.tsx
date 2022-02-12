6a8c2d36a892ba66054236e0a1f3b3e2f95117f2
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-12 19:01:30 +0200

    feat: add password field

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,19 +1,27 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import SettingsInputAntennaIcon from '@mui/icons-material/SettingsInputAntenna';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import { FormWrapper, Title, TitleBlock } from './styles';
 
-const loginSchema = yup
+interface LoginForm {
+  login: string;
+  password: string;
+}
+
+const loginFormSchema = yup
   .object({
     login: yup.string().length(6, 'Login should contain 6 characters').required(),
     password: yup.string().required(),
@@ -25,10 +33,15 @@ const Login: FC = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(loginSchema),
+  } = useForm<LoginForm>({
+    resolver: yupResolver(loginFormSchema),
   });
   const onSubmit = (data: any) => console.log(data);
+
+  const [isShowPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword((showPassword) => !showPassword);
+  };
 
   return (
     <Container maxWidth="sm">
@@ -44,12 +57,33 @@ const Login: FC = () => {
             label="Activation login"
             fullWidth
             margin="normal"
-            error={!!errors['login']}
-            helperText={errors['login']?.message}
+            error={!!errors.login}
+            helperText={errors.login?.message}
             {...register('login', { required: true })}
           />
+          <TextField
+            label="Password"
+            type={isShowPassword ? 'text' : 'password'}
+            fullWidth
+            margin="normal"
+            error={!!errors.login}
+            helperText={errors.login?.message}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={toggleShowPassword}
+                    edge="end">
+                    {isShowPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            {...register('password', { required: true })}
+          />
           <Button variant="contained" onClick={handleSubmit(onSubmit)}>
-            Submit
+            Sign In
           </Button>
         </form>
       </Box>
