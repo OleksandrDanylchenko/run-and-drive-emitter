23fd0e47d6739584b1d99f0ed8abe649cca75408
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-12 11:41:08 +0200

    refactor: add usage of the flexbox mixin for the login page styles

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,14 +1,17 @@
 import { css } from '@emotion/react';
+import { flexbox } from '@styles/mixins';
 import { pxToRem } from '@styles/utils';
 
-export const TitleBlock = css({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  gap: pxToRem('20px'),
-  padding: pxToRem('15px'),
-});
+export const TitleBlock = css`
+  ${flexbox({
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: pxToRem('20px'),
+  })}
 
-export const Title = css({
-  fontSize: pxToRem('35px'),
-});
+  padding-top: ${pxToRem('15px')};
+`;
+
+export const Title = css`
+  font-size: ${pxToRem('35px')};
+`;
