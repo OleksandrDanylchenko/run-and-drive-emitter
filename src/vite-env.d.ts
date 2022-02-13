004fbe9b90129d56e61b0f84409b86d0a470e0c4
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-13 23:18:19 +0200

    fix: suppress `Theme`

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,6 @@
 import { Theme as MaterialUITheme } from '@mui/material';
 
 declare module '@emotion/react' {
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line
   interface Theme extends MaterialUITheme {}
 }
