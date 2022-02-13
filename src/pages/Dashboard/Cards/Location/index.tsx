3a932425a60fdce7614f76beac6a717ae153db89
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-13 11:23:51 +0200

    feat: add basic location card

---
+++ src/pages/Dashboard/Cards/Location/index.tsx
@@ -0,0 +1,60 @@
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Collapse from '@mui/material/Collapse';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import React, { FC, useState } from 'react';
+
+import { LocationTable } from './styles';
+
+const LocationCard: FC = () => {
+  const [isShowMap, setShowMap] = useState(false);
+  const toggleMapShow = () => {
+    setShowMap((showMap) => !showMap);
+  };
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          Location
+        </Typography>
+        <TableContainer css={LocationTable}>
+          <Table aria-label="Location table">
+            <TableHead>
+              <TableCell align="center">Latitude</TableCell>
+              <TableCell align="center">Longitude</TableCell>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align="center">59</TableCell>
+                <TableCell align="center">2</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </CardContent>
+      <Collapse in={isShowMap} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Here is the map!
+          </Typography>
+        </CardContent>
+      </Collapse>
+      <CardActions disableSpacing sx={{ justifyContent: 'end' }}>
+        <Button size="small" onClick={toggleMapShow}>
+          {isShowMap ? 'Hide map' : 'Show on map'}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default LocationCard;
---
+++ src/pages/Dashboard/Cards/Location/styles.ts
@@ -0,0 +1,6 @@
+import { css, Theme } from '@emotion/react';
+
+export const LocationTable = (theme: Theme) => css`
+  border: 1px solid ${theme.palette.grey.A200};
+  border-radius: 4px;
+`;
