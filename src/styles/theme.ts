2d1d35ecaf4179ea9de0f2aff8eb73ec8f6c1126
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-13 18:13:51 +0200

    refactor: make `wave` default animation for the skeleton

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,9 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   components: {
     MuiSkeleton: {
+      defaultProps: {
+        animation: 'wave',
+      },
       styleOverrides: {
         rectangular: {
           borderRadius: '4px',
