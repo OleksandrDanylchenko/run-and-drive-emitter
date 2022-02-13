77fa527f8327f06382f3e4286f61656128f97718
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-13 11:23:28 +0200

    feat: add basic location card

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+import { Theme as MaterialUITheme } from '@mui/material';
+
+declare module '@emotion/react' {
+  // eslint-disable-next-line no-unused-vars
+  interface Theme extends MaterialUITheme {}
+}
