8cab6e0b7551ad14716c409b8cb14b247608857d
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-02-13 23:16:11 +0200

    feat: add password form for the deactivation modal

--- src/pages/Settings/DeactivateModal.tsx
+++ src/pages/Settings/DeactivateModal.tsx
@@ -1,18 +1,53 @@
+import PasswordInput from '@components/PasswordInput';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import { BindingAction } from '@models/functions';
+import CancelIcon from '@mui/icons-material/Cancel';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { FC } from 'react';
+import { LoginForm } from '@pages/Login';
+import { useMockLogin } from '@pages/Login/useMockLogin';
+import { isEmpty } from '@utils/index';
+import { passwordSchema } from '@utils/validation';
+import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 interface DeactivateModalProps {
   open: boolean;
   onClose: BindingAction;
 }
 
+const deactivateSchema = yup.object({ password: passwordSchema }).required();
+
 const DeactivateModal: FC<DeactivateModalProps> = ({ open, onClose }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: 'onBlur',
+    resolver: yupResolver(deactivateSchema),
+    defaultValues: {
+      password: '',
+    },
+  });
+
+  const [isLoading, onSubmit, error] = useMockLogin();
+
+  const handleCancel = () => {
+    onClose();
+    reset();
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Deactivate emitter</DialogTitle>
@@ -20,12 +55,32 @@ const DeactivateModal: FC<DeactivateModalProps> = ({ open, onClose }) => {
         <DialogContentText>
           Provide the password to confirm the deactivation
         </DialogContentText>
+        <PasswordInput
+          margin="dense"
+          variant="standard"
+          error={!!errors.password}
+          helperText={errors.password?.message}
+          {...register('password', { required: true })}
+        />
       </DialogContent>
+      <Collapse in={!!error}>
+        <Alert severity="error">
+          <AlertTitle>Deactivation failed</AlertTitle>
+          {error}
+        </Alert>
+      </Collapse>
       <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button variant="contained" onClick={onClose}>
+        <Button onClick={handleCancel}>Cancel</Button>
+        <LoadingButton
+          variant="contained"
+          color="error"
+          endIcon={<CancelIcon />}
+          disabled={!isEmpty(errors)}
+          loading={isLoading}
+          loadingPosition="end"
+          onClick={handleSubmit(onSubmit)}>
           Deactivate
-        </Button>
+        </LoadingButton>
       </DialogActions>
     </Dialog>
   );
