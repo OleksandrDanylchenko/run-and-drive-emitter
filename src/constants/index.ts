c203f44b299aef0dabe449f34f7f968eac04f1b7
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-12 21:34:07 +0300

    feat: add test trip preview on the map

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const API_HOST = import.meta.env.VITE_API_HOST;
 export const IS_DEVELOPMENT = import.meta.env.DEV;
 export const IS_PROD = import.meta.env.PROD;
+
+export const GOOGLE_MAPS_KEY = import.meta.env.VITE_GOOGLE_MAP_KEY;
--- src/pages/Settings/Sections/TestTrips/TripSelector.tsx
+++ src/pages/Settings/Sections/TestTrips/TripSelector.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useState, useMemo } from 'react';
+import React, { FC, useState, useMemo, useCallback, useEffect } from 'react';
 
+import { css } from '@emotion/react';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import InputLabel from '@mui/material/InputLabel';
@@ -11,9 +12,12 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
+import { Marker } from '@react-google-maps/api';
+import { GoogleMap } from 'run-and-drive-lib/components';
 import { Car } from 'run-and-drive-lib/models';
 import { toMeters } from 'run-and-drive-lib/utils';
 
+import { GOOGLE_MAPS_KEY } from '@constants/index';
 import { TestTripSummary } from '@models/api';
 
 interface LocationTableProps {
@@ -59,8 +63,9 @@ const TripSelector: FC<LocationTableProps> = ({ trips, car }) => {
                 <TableCell align="center">{toMeters(chosenTrip.totalDistance)}</TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>Hello!</TableCell>
-                <TableCell align="center">gsdgsdg</TableCell>
+                <TableCell colSpan={2} css={TripsDetailsMapCell}>
+                  <TripDetailsMap chosenTrip={chosenTrip} />
+                </TableCell>
               </TableRow>
             </TableBody>
           </Table>
@@ -69,5 +74,65 @@ const TripSelector: FC<LocationTableProps> = ({ trips, car }) => {
     </Stack>
   );
 };
+
+const TripDetailsMap: FC<{ chosenTrip: TestTripSummary }> = ({ chosenTrip }) => {
+  const { startLocation, endLocation } = chosenTrip;
+
+  const [mapInstance, setMapInstance] = useState<google.maps.Map>();
+
+  useEffect(() => {
+    if (!mapInstance) return;
+
+    const start = new google.maps.LatLng(startLocation);
+    const end = new google.maps.LatLng(endLocation);
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(start);
+    bounds.extend(end);
+    mapInstance.fitBounds(bounds);
+  }, [endLocation, mapInstance, startLocation]);
+
+  const getMarkerIcon = useCallback<(type: 'start' | 'end') => google.maps.Icon>(
+    (type) => {
+      const markerSize = 33;
+
+      const markerUrl =
+        type === 'start'
+          ? 'https://i.ibb.co/x7cwmS1/red-marker.png'
+          : 'https://i.ibb.co/phXQnbk/blue-marker.png';
+
+      return {
+        url: markerUrl,
+        scaledSize: new google.maps.Size(markerSize, markerSize),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(markerSize / 2, markerSize / 2),
+      };
+    },
+    [],
+  );
+
+  return (
+    <GoogleMap
+      apiKey={GOOGLE_MAPS_KEY}
+      onMapLoad={(map: google.maps.Map) => setMapInstance(map)}
+      css={TripsDetailsMap}>
+      {mapInstance && (
+        <>
+          <Marker key="start" position={startLocation} icon={getMarkerIcon('start')} />
+          <Marker key="end" position={endLocation} icon={getMarkerIcon('end')} />
+        </>
+      )}
+    </GoogleMap>
+  );
+};
+
+const TripsDetailsMapCell = css`
+  padding: 0;
+  height: 320px;
+`;
+
+const TripsDetailsMap = css`
+  width: 100%;
+  height: 100%;
+`;
 
 export default TripSelector;
